2121673627e287b47054509a997cc369e6fc470b
saif-lesnar <saif_lesnar@outlook.com>
2019-11-05 21:15:25 +0600

    Update Controller implementation in progress

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace DatingApp.API.Controllers
@@ -36,6 +37,22 @@ namespace DatingApp.API.Controllers
             var user = await _repo.GetUser(id);
             var userToReturn = _mapper.Map<UserForDetailsDto>(user);
             return Ok(userToReturn);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id, UserUpdateDto userUpdateDto)
+        {
+            if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+            }
+
+            var userFromRepo = await _repo.GetUser(id);
+
+            _mapper.Map(userUpdateDto, userFromRepo);
+            if (await _repo.SaveAll())
+                return NoContent();
+            throw new Exception($"Updating user {id} failed on save");
+
         }
     }
 }
