558f6472aa8f2e423da27ddb2c3ad79694f82e59
saif-lesnar <saif_lesnar@outlook.com>
2019-11-11 15:11:50 +0600

    Api for setting main photo

--- Data/DatingRepository.cs
+++ Data/DatingRepository.cs
@@ -24,6 +24,11 @@ namespace DatingApp.API.Data
             _context.Remove(entity);
         }
 
+        public async Task<Photo> GetMainPhotoForUser(int userid)
+        {
+            return await _context.Photos.Where(u => u.UserId == userid).FirstOrDefaultAsync(p => p.IsMain);
+        }
+
         public async Task<Photo> GetPhoto(int id)
         {
             var photo = await _context.Photos.FirstOrDefaultAsync(p => p.Id == id);
